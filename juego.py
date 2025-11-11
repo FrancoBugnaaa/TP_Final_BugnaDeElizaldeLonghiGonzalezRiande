fc74dc9748851f4b2f1e9d79534b7a4980bdf98b
luciagonzalez2 <lgonzalezriande@udesa.edu.ar>
2025-11-11 13:14:52 -0300

    inicio juego

--- juego.py
+++ juego.py
@@ -6,19 +6,68 @@ pygame.init()
 #Creacion de la ventana del juego
 screen = pygame.display.set_mode((1000,600))
 
+
+clock = pygame.time.Clock()
+
+#Fondo
+
+fondo = pygame.image.load("background.png")
+
 #Titulo e iconos
 pygame.display.set_caption("The Flappy Bird")
 icon = pygame.image.load("/Users/marialaurariande/Downloads/pajarito.png")
 pygame.display.set_icon(icon)
+
+#Imagen del flappy bird
+
+imagenPajarito = pygame.image.load("bird.png")
+jugadorX = 500
+jugadorY = 300
+cambios_jugadorX = 0
+velocidad_y = 0
+
+
+def jugador(x,y):
+    screen.blit(imagenPajarito, (x, y))
+
+
 #Game Loop
-
-
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    #RGB - Colores - Rojo, Verde, Azul        
-    screen.fill((51,255,255))
+    clock.tick(60)
+
+    #RGB - Colores - Rojo, Verde, Azul  
+    screen.fill((0,0,0))
+
+    #Imagen de fondo
+    screen.blit(fondo, (0,0))
+    
+    for event in pygame.event.get():
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+            if event.key == pygame.K_SPACE:
+                velocidad_y = -10
+        
+        elif event.type == pygame.QUIT:
+            running = False
+    
+    velocidad_y += 0.5
+    jugadorY += velocidad_y
+
+    if jugadorY < 0:
+            jugadorY = 0
+            velocidad_y = 0
+    
+    if jugadorY > 600 - imagenPajarito.get_height():
+            jugadorY = 600 - imagenPajarito.get_height()
+            velocidad_y = 0
+            
+            
+    
+    jugador(jugadorX, jugadorY)
     pygame.display.update()
