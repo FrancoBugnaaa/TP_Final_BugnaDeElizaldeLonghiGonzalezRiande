d141d037980283338cb88e1e4fe261f87aeae126
luciagonzalez2 <lgonzalezriande@udesa.edu.ar>
2025-11-11 15:10:26 -0300

    Agregando tuberias

--- juego.py
+++ juego.py
@@ -20,14 +20,35 @@ pygame.display.set_icon(icon)
 
 #Imagen del flappy bird
 
-imagenPajarito = pygame.image.load("bird.png")
+imagenPajarito = pygame.image.load("bird.png").convert_alpha()
 jugadorX = 500
 jugadorY = 300
 cambios_jugadorX = 0
 velocidad_y = 0
 
+tubo_abajo_img = pygame.image.load("tub_abajo_100.png").convert_alpha()
+tubo_arriba_img = pygame.image.load("tub_arriba_100.png").convert_alpha()
+
+tubo_x = 350
+hueco = 200
+hueco_y = 300
+
+tubo_abajo_rect = tubo_abajo_img.get_rect()
+tubo_abajo_rect.x = tubo_x
+tubo_abajo_rect.y = hueco_y + (hueco // 2)
+
+tubo_arriba_rect = tubo_arriba_img.get_rect()
+tubo_arriba_rect.x = tubo_x
+tubo_arriba_rect.y = hueco_y - (hueco // 2) - tubo_arriba_rect.height
+
+
+
+
 
 def jugador(x,y):
+    
+    screen.blit(tubo_abajo_img, tubo_abajo_rect)
+    screen.blit(tubo_arriba_img, tubo_arriba_rect)
     screen.blit(imagenPajarito, (x, y))
 
 
